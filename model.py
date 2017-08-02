891756fc533d7467c8ad912693bd04c67cd55921
p-flock <peter.flockhart@gmail.com>
2017-08-02 14:57:26 -0400

    more concise model saving

--- model.py
+++ model.py
@@ -23,9 +23,10 @@ model.fit(data, labels, epochs=10, batch_size=32)
 model.summary()
 
 # save model as json and save weights as h5 file
-model_as_json = model.to_json()
-with open("chess_model.json", "r") as json_file:
-    json_file.write(model_as_json)
+# model_as_json = model.to_json()
+# with open("chess_model.json", "r") as json_file:
+    # json_file.write(model_as_json)
+model.save("chess_model.h5")
 
 #weights
 model.save_weights("model_weights.h5")
